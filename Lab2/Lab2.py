9999ae0a654f66bee358afd06688c475d26ccace
TopD9160 <mw5228vd@go.minnstate.edu>
2022-09-07 11:57:51 -0500

    changes

--- Lab2/Lab2.py
+++ Lab2/Lab2.py
@@ -8,7 +8,7 @@ human_age = float(input("how old are you?"))
 dog_year = 7 # 1 human year is 7 dog
 human_in_dog_years = dog_year * human_age
 
-print("an human aged",human_age,"year old human would be",human_in_dog_years ,"years")
+print("an human aged",human_age,"year old human in dog years would be",human_in_dog_years ,"years")
 
 
 ########
@@ -16,11 +16,11 @@ print("an human aged",human_age,"year old human would be",human_in_dog_years ,"y
 cat_year = 9
 human_in_cat_years = cat_year * human_age #cat year human age * 9
 
-print("an human aged",human_age,"year old human would be",human_in_cat_years,"years")
+print("an human aged",human_age,"year old human in cat years would be",human_in_cat_years,"years")
 
 
 ########
 #Horse Years
 horse_years = 3 * ((((human_age**2 - 47)/7))+12)
 
-print("an human aged",human_age,"year old human would be",horse_years,"years")
+print("an human aged",human_age,"year old human in horse years would be",horse_years,"years")
