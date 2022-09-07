4ee1c688041dc93860935616d7c9d581a385eeea
TopD9160 <mw5228vd@go.minnstate.edu>
2022-09-07 00:01:33 -0500

    changes

--- Lab2/humanandog.py
+++
@@ -1,9 +0,0 @@
-dog_year = 7 # 1 human year is 7 dog
-
-#asking for human age
-human_age = int(input("how old are you?"))
-
-human_in_dog_years = dog_year * human_age
-
-#old  print(dog_year * human_age)
-print("an age ",human_age,"year old human should be ",dog_year * 45,"in dog years")
